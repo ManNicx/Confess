b03e8cfde10073cd62fb888c703e7030d429b115
ManNicx <113735662+ManNicx@users.noreply.github.com>
2023-02-12 20:21:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  alert("Warning!!");
+  alert("What if?");
+  alert("May Gusto ako sayu?");
+  alert("What if?");
+  alert("Tayo talaga?");
+  alert("What if?");
+  alert("Tayo talaga sa Isat-isa?");
+  alert("What if?");
+  alert("Tang-inang What if nayan")
+  $(".container").show(100);
+  
+  
+  
+})
